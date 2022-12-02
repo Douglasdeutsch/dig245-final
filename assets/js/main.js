640f746ffed8b5089cee2f5297a44ff5179ae183
Douglas Deutsch <dodeutsch@davidson.edu>
2022-12-02 13:35:11 -0500

    commit-3
    This commit changed the names of the buttons for the variables in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@
 /* javascript */
 
 
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('btn btn-primary');
+const searchInput = document.getElementById('form1');
 searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   document.location.href="assets/browse-page.html";
