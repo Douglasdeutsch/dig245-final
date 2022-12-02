c5a783d35ebadf5f17b13e791491f714d17f1ecb
Douglas Deutsch <dodeutsch@davidson.edu>
2022-12-02 13:38:56 -0500

    commit 4
    Here I added a console.log statement to see where my code was going wrong

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,6 @@ const searchInput = document.getElementById('form1');
 searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   document.location.href="assets/browse-page.html";
+  console.log("clicked"+"; my search was "+ inputValue);
 
 });
